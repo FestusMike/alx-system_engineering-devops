b38491e155f82458d56ea21e1fdc8dac8dd576d7
FestusMike <festusmike98@gmail.com>
2023-05-03 09:23:40 -0700

    changes

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Script should output: [SENDER],[RECEIVER],[FLAGS]
+# script should output: [SENDER],[RECEIVER],[FLAGS]
 
 puts ARGV[0].scan(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*?)\]/).join(",")
